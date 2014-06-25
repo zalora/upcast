c786fb53a04281bf00edd6131ead6ed55f040bef
Vladimir Kirillov <proger@wilab.org.ua>
2014-06-25 16:44:53 +0300

    command: properly dispose resources, properly ignore stderr

--- src/Upcast/Command.hs
+++ src/Upcast/Command.hs
@@ -3,21 +3,24 @@
 module Upcast.Command where
 
 import Control.Monad.Trans (liftIO)
-import Control.Monad (ap, join)
+import Control.Monad (ap, join, (<=<))
 import System.FilePath (FilePath)
 import System.Process (createProcess, waitForProcess, CreateProcess(..), CmdSpec(..), StdStream(..))
-import GHC.IO.Handle (hClose, Handle)
-import System.IO (hSetBuffering, BufferMode(..))
+import GHC.IO.Handle (hClose, Handle, hSetBinaryMode)
+import System.IO (hSetBuffering, BufferMode(..), stdout, IOMode(..), openFile)
 import System.Posix.IO (createPipe, fdToHandle)
+import Data.Maybe (maybe)
+import Control.Monad.Trans.Resource (runResourceT, liftResourceT, MonadResource(..))
 
 import Data.ByteString as BS
 
 import Data.Conduit
+import qualified Data.Conduit.List as CL
 import Blaze.ByteString.Builder
 
 import Upcast.Interpolate (n)
 
-type ChunkSource = Source IO (Flush Builder)
+type ChunkSource m = Source m (Flush Builder)
 type Key = FilePath
 
 data Local = Local deriving (Show)
@@ -25,15 +28,30 @@ data Remote = Remote Key String deriving (Show)
 
 data Command a = Cmd a String deriving (Show)
 
+fgrun c = run c $$ awaitForever $ liftIO . BS.putStr . chunk
 
-run :: Command Local -> IO ChunkSource
-run (Cmd Local s) = go s
-  where
-    go = fmap sourceHandle . runCreateProcess . cmd
+fgconsume (Cmd Local s) = (runResourceT $ proc $$ CL.consume) >>= return . concat
+    where
+      proc = bracketP (openFile "/dev/null" WriteMode) hClose $ \null -> roProcessSource (cmd s) Nothing (Just null)
+      concat = BS.concat . fmap chunk
+
+chunk (Chunk a) = toByteString a
+chunk _ = ""
+
+run (Cmd Local s) = roProcessSource (cmd s) Nothing Nothing
 
 ssh :: Command Remote -> Command Local
 ssh (Cmd (Remote key host) cmd) =
     Cmd Local [n|ssh -i #{key} -x root@#{host} -- '#{cmd}'|]
+
+sshMaster controlPath (Remote key host) =
+    Cmd Local [n|ssh -x root@#{host} -S #{controlPath} -M -N -f -oNumberOfPasswordPrompts=0 -oServerAliveInterval=60 -i #{key}|]
+
+sshMasterExit controlPath (Remote _ host) =
+    Cmd Local [n|ssh root@#{host} -S #{controlPath} -O exit|]
+
+sshFast controlPath (Cmd (Remote key host) cmd) =
+    Cmd Local [n|ssh -oControlPath=#{controlPath} -i #{key} -x root@#{host} -- '#{cmd}'|]
 
 
 cmd :: String -> CreateProcess
@@ -48,30 +66,39 @@ cmd s = CreateProcess { cmdspec = ShellCommand s
                       , delegate_ctlc = False
                       }
 
-runCreateProcess :: CreateProcess -> IO Handle
-runCreateProcess proc = do
-    (rh, wh) <- createPipeHandle
-    hSetBuffering rh LineBuffering
-    hSetBuffering wh LineBuffering
-    (Just stdin, _, _, _) <- createProcess proc{std_out = UseHandle wh, std_err = UseHandle wh}
-    hClose stdin
-    return rh
+roProcessSource proc stdout stderr =
+  let
+    stdout' d = maybe d id stdout
+    stderr' d = maybe d id stderr
+  in do
+    handles@(rh, wh) <- liftIO createPipeHandle
+    (Just stdin, _, _, _) <- liftIO $ createProcess proc { std_out = UseHandle $ stdout' wh
+                                                         , std_err = UseHandle $ stderr' wh }
+    liftIO $ hClose stdin
+    bracketP (return handles) (mapMBoth_ hClose) (sourceHandle . fst)
 
 mapMBoth :: Monad m => (t -> m a) -> (t, t) -> m (a, a)
 mapMBoth f (a, b) = return (,) `ap` f a `ap` f b
 
+mapMBoth_ f (a, b) = f a >> f b  >> return ()
+
 createPipeHandle :: IO (Handle, Handle)
 createPipeHandle = join $ fmap (mapMBoth fdToHandle) createPipe
 
-sourceHandle :: Handle -> ChunkSource
-sourceHandle h = do
-    loop
+sourceHandle h = loop
   where
     loop = do
-        x <- liftIO $ BS.hGetSome h 64
+        x <- liftIO $ BS.hGetSome h 128
         if BS.null x
             then return ()
             else do
               yield $ Chunk $ fromByteString x
               yield Flush
               loop
+
+applyColor index s = "\ESC[1;" ++ color ++ "m" ++ s ++ "\ESC[0m"
+  where
+    color = show $ (31 + (index `mod` 7))
+
+colorize = applyColor 5
+
