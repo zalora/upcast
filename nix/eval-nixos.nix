a0768beb5fb7a7555b035eca0169db7fba5f47ff
Vladimir Kirillov <proger@wilab.org.ua>
2014-12-25 16:02:09 +0200

    nix: introduce eval-nixos (start decoupling images and infra)

---
+++ nix/eval-nixos.nix
@@ -0,0 +1,22 @@
+exprs:
+let
+  inherit (import <nixpkgs/lib>) mkDefault evalModules isList;
+
+  pkgs-mod = rec {
+    _file = ./eval-nixos.nix;
+    key = _file;
+    config = {
+      nixpkgs.system = mkDefault "x86_64-linux";
+    };
+  };
+
+  eval = evalModules {
+    modules =
+      [ pkgs-mod ] ++
+      (import <nixpkgs/nixos/modules/module-list.nix>) ++
+      (if isList exprs then exprs else [ exprs ]);
+  };
+in {
+  inherit (eval) config options;
+  inherit (eval.config.__internal.args) pkgs;
+}
