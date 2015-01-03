eaea80ac77517bdcdcd57abd53f6a3e380ce79bb
Vladimir Kirillov <proger@wilab.org.ua>
2015-01-03 17:41:06 +0200

    14.10 is actually 14.12

--- nix/aws/ec2-amis.nix
+++ nix/aws/ec2-amis.nix
@@ -1,4 +1,4 @@
 {
-  "14.10"."ap-southeast-1".hvm = "ami-65f5db37";
-  "14.10"."eu-west-1".hvm = "ami-4a6ad03d";
+  "14.12"."ap-southeast-1".hvm = "ami-65f5db37";
+  "14.12"."eu-west-1".hvm = "ami-4a6ad03d";
 }
