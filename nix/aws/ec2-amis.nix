e965b33371258ac0f1f680b013339450076a8457
Vladimir Kirillov <proger@wilab.org.ua>
2015-01-06 13:18:52 +0200

    updated AMIs (linux 3.18 including overlayfs)

--- nix/aws/ec2-amis.nix
+++ nix/aws/ec2-amis.nix
@@ -1,4 +1,4 @@
 {
-  "14.12"."ap-southeast-1".hvm = "ami-65f5db37";
-  "14.12"."eu-west-1".hvm = "ami-4a6ad03d";
+  "14.12"."ap-southeast-1".hvm = "ami-8f143ddd";
+  "14.12"."eu-west-1".hvm = "ami-193bbf6e";
 }
