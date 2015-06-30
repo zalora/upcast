badfa6af50d98091283a57e53287571d1ae5e0fe
Vladimir Kirillov <proger@wilab.org.ua>
2015-06-30 11:06:31 -0700

    travis: use upcast from ~/.local/bin

--- travis.sh
+++ travis.sh
@@ -4,6 +4,7 @@ upcast="upcast-$TRAVIS_BUILD_NUMBER"
 
 ~/bin/stack build
 ~/bin/stack install
-cp "$(~/bin/stack path | awk -F: '/^local-install-root/{print $2}')bin/upcast" $upcast
+#cp "$(~/bin/stack path | awk -F: '/^local-install-root/{print $2}')bin/upcast" $upcast
+cp ~/.local/bin/upcast $upcast
 strip -p --strip-unneeded --remove-section=.comment -o $upcast
 upx $upcast
