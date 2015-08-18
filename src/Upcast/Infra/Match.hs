815f0ef0a94faf04341c72729425729e1f16c2bb
Vladimir Kirillov <proger@wilab.org.ua>
2015-08-18 21:19:16 +0300

    upgrade to amazonka 1.0

--- src/Upcast/Infra/Match.hs
+++ src/Upcast/Infra/Match.hs
@@ -14,9 +14,10 @@ import           Control.Applicative
 import           Control.Lens hiding ((.=))
 
 import           Control.Monad hiding (sequence)
-import           Control.Monad.Catch (MonadCatch)
+import           Control.Monad.Catch (MonadCatch, try, throwM)
 import           Control.Monad.Error.Class (MonadError, throwError)
-import           Control.Monad.Trans.AWS (MonadAWS, AWSRequest, AWSPager)
+import           Control.Monad.Free.Class (MonadFree)
+import           Control.Monad.Trans.AWS (AWSRequest, AWSPager, AWST)
 import qualified Control.Monad.Trans.AWS as AWS
 import           Control.Monad.Trans.Resource
 
@@ -33,11 +34,13 @@ import           Data.Conduit (($$), (=$=))
 import qualified Data.Conduit as C
 import qualified Data.Conduit.List as CL
 
+import           Network.AWS (MonadAWS, Error)
 import qualified Network.AWS.EC2 as EC2
 import qualified Network.AWS.EC2.Types as EC2
 import qualified Network.AWS.ELB as ELB
 import qualified Network.AWS.ELB.Types as ELB
-import           Network.AWS.Error (awsErrorCode)
+import           Network.AWS.Free (Command)
+import           Network.AWS.Pager
 
 import           Upcast.Infra.NixTypes
 import           Upcast.Infra.Types
@@ -71,49 +74,49 @@ class AWSMatchRequest infra where
 instance AWSMatchRequest Ec2instance where
   type Rq Ec2instance = EC2.DescribeInstances
 
-  matchRequest _ tags = EC2.describeInstances & EC2.di1Filters .~ alive:(filters tags)
+  matchRequest _ tags = EC2.describeInstances & EC2.diiFilters .~ alive:(filters tags)
     where
       alive = EC2.filter' "instance-state-name" & EC2.fValues .~ ["pending", "running", "stopped"]
-  matchIds ids _ = EC2.describeInstances & EC2.di1InstanceIds .~ ids
+  matchIds ids _ = EC2.describeInstances & EC2.diiInstanceIds .~ ids
 
 instance AWSExtractIds EC2.DescribeInstancesResponse where
   extractIds resp = do
-    rs <- resp ^. EC2.dirReservations
+    rs <- resp ^. EC2.dirsReservations
     inst <- rs ^. EC2.rInstances
-    return $ inst ^. EC2.i1InstanceId
+    return $ inst ^. EC2.insInstanceId
 
 
 instance AWSMatchRequest Ec2keypair where
   type Rq Ec2keypair = EC2.DescribeKeyPairs
-  matchRequest _ tags = EC2.describeKeyPairs & EC2.dkp1Filters .~ filters tags
-  matchIds ids _ = EC2.describeKeyPairs & EC2.dkp1Filters .~ filterIds "key-name" ids
+  matchRequest _ tags = EC2.describeKeyPairs & EC2.dkpsFilters .~ filters tags
+  matchIds ids _ = EC2.describeKeyPairs & EC2.dkpsFilters .~ filterIds "key-name" ids
 
 instance AWSExtractIds EC2.DescribeKeyPairsResponse where
   extractIds resp = do
-    kpi <- resp ^. EC2.dkprKeyPairs
+    kpi <- resp ^. EC2.dkprsKeyPairs
     maybeToList $ kpi ^. EC2.kpiKeyName
 
 
 instance AWSMatchRequest Ebs where
   type Rq Ebs = EC2.DescribeVolumes
   matchRequest Ebs{..} tags = EC2.describeVolumes &
-                              EC2.dv2Filters .~ filters (("Name", ebs_name):tags)
-  matchIds ids _ = EC2.describeVolumes & EC2.dv2VolumeIds .~ ids
+                              EC2.desFilters .~ filters (("Name", ebs_name):tags)
+  matchIds ids _ = EC2.describeVolumes & EC2.desVolumeIds .~ ids
 
 instance AWSExtractIds EC2.DescribeVolumesResponse where
   extractIds resp = do
-    vol <- resp ^. EC2.dvrVolumes
+    vol <- resp ^. EC2.dvvrsVolumes
     return $ vol ^. EC2.vVolumeId
 
 
 instance AWSMatchRequest Ec2sg where
   type Rq Ec2sg = EC2.DescribeSecurityGroups
-  matchRequest _ tags = EC2.describeSecurityGroups & EC2.dsg1Filters .~ filters tags
-  matchIds ids _ = EC2.describeSecurityGroups & EC2.dsg1GroupIds .~ ids
+  matchRequest _ tags = EC2.describeSecurityGroups & EC2.dsgsFilters .~ filters tags
+  matchIds ids _ = EC2.describeSecurityGroups & EC2.dsgsGroupIds .~ ids
 
 instance AWSExtractIds EC2.DescribeSecurityGroupsResponse where
   extractIds resp = do
-    sg <- resp ^. EC2.dsgrSecurityGroups
+    sg <- resp ^. EC2.dsgrsSecurityGroups
     return $ sg ^. EC2.sgGroupId
 
 
@@ -124,19 +127,19 @@ instance AWSMatchRequest Ec2subnet where
 
 instance AWSExtractIds EC2.DescribeSubnetsResponse where
   extractIds resp = do
-    subnet <- resp ^. EC2.dsrSubnets
-    return $ subnet ^. EC2.s1SubnetId
+    subnet <- resp ^. EC2.dsrsSubnets
+    return $ subnet ^. EC2.subSubnetId
 
 
 instance AWSMatchRequest Ec2vpc where
-  type Rq Ec2vpc = EC2.DescribeVpcs
-  matchRequest _ tags = EC2.describeVpcs & EC2.dv1Filters .~ filters tags
-  matchIds ids _ = EC2.describeVpcs & EC2.dv1VpcIds .~ ids
+  type Rq Ec2vpc = EC2.DescribeVPCs
+  matchRequest _ tags = EC2.describeVPCs & EC2.dvsFilters .~ filters tags
+  matchIds ids _ = EC2.describeVPCs & EC2.dvsVPCIds .~ ids
 
-instance AWSExtractIds EC2.DescribeVpcsResponse where
+instance AWSExtractIds EC2.DescribeVPCsResponse where
   extractIds resp = do
-    vpc <- resp ^. EC2.dvrVpcs
-    return $ vpc ^. EC2.vpcVpcId
+    vpc <- resp ^. EC2.dvrsVPCs
+    return $ vpc ^. EC2.vpcVPCId
 
 
 instance AWSMatchRequest Elb where
@@ -146,11 +149,11 @@ instance AWSMatchRequest Elb where
 
 instance AWSExtractIds ELB.DescribeLoadBalancersResponse where
   extractIds resp = do
-    elb <- resp ^. ELB.dlbrLoadBalancerDescriptions
+    elb <- resp ^. ELB.dlbrsLoadBalancerDescriptions
     maybeToList $ elb ^. ELB.lbdLoadBalancerName
 
 
-instance AWSPager EC2.DescribeVpcs where page _ _ = Nothing
+instance AWSPager EC2.DescribeVPCs where page _ _ = Nothing
 instance AWSPager EC2.DescribeSubnets where page _ _ = Nothing
 instance AWSPager EC2.DescribeSecurityGroups where page _ _ = Nothing
 instance AWSPager EC2.DescribeKeyPairs where page _ _ = Nothing
@@ -158,26 +161,19 @@ instance AWSPager EC2.DescribeVolumes where page _ _ = Nothing
 
 
 class AWSExtractResponse infra where
-  extractResponse :: MonadError AWS.Error m => proxy infra -> AWS.Response (Rq infra) -> m [ResourceId]
+  extractResponse :: MonadThrow m => proxy infra -> Either Error [AWS.Rs (Rq infra)]-> m [ResourceId]
 
   default extractResponse :: (AWSExtractIds (AWS.Rs (Rq infra)),
-                              Show (AWS.Er (AWS.Sv (Rq infra))),
-                              MonadError AWS.Error m)
+                              MonadThrow m)
                           => proxy infra
-                          -> AWS.Response (Rq infra)
+                          -> Either Error [AWS.Rs (Rq infra)]
                           -> m [ResourceId]
-  extractResponse _ response = case response of
-    Left a -> AWS.throwAWSError a
-    Right r -> return (extractIds r)
+  extractResponse _ = either throwM (return . (>>= extractIds))
 
 instance AWSExtractResponse Elb where
   extractResponse _ response = case response of
-    Left error@(AWS.ServiceError _ _ elbError) ->
-      case elbError  ^. AWS.restCode of
-        Just "LoadBalancerNotFound" -> return []
-        _ -> AWS.throwAWSError error
-    Left error -> AWS.throwAWSError error
-    Right r -> return (extractIds r)
+    Left e -> maybe (throwM e) (return . const []) (e ^? ELB._AccessPointNotFoundException)
+    Right r -> return (r >>= extractIds)
 
 instance AWSExtractResponse Ec2sg
 instance AWSExtractResponse Ec2subnet
@@ -186,38 +182,35 @@ instance AWSExtractResponse Ec2instance
 instance AWSExtractResponse Ec2vpc
 instance AWSExtractResponse Ebs
 
-
 matchTags ::
   forall infra m.
   (CanMatch infra,
-   MonadAWS m,
    MonadCatch m,
-   MonadError AWS.Error m,
-   MonadResource m)
+   MonadResource m,
+   MonadFree Command m)
   => infra
   -> Tags
   -> m [ResourceId]
-matchTags infra tags = C.runConduit conduit
+matchTags infra tags = try (C.runConduit conduit) >>= extractResponse (Proxy :: Proxy infra)
   where
-    conduit = AWS.paginateCatch (matchRequest infra tags) =$=
-              CL.concatMapM (extractResponse (Proxy :: Proxy infra)) =$=
-              CL.consume
+    conduit = AWS.paginate (matchRequest infra tags) =$= CL.consume
 
+toDiscovery :: [Text] -> Either DiscoveryError Text
 toDiscovery [one] = Right one
 toDiscovery [] = Left NotFound
 toDiscovery many = Left (Ambiguous many)
 
 discover i t = toDiscovery <$> matchTags i t
 
-filters tags = toFilter <$> tags --("created-using", "upcast"):tags
+filters tags = toFilter <$> tags
   where toFilter (k, v) = EC2.filter' (mconcat ["tag:", k]) & EC2.fValues .~ [v]
 
 filterIds tag ids = [EC2.filter' tag & EC2.fValues .~ ids]
 
 matchInfras :: Infras -> IO Value
 matchInfras infras@Infras{..} = object <$> do
-  env <- AWS.getEnv region AWS.Discover
-  expectRight $ AWS.runAWST env $
+  env <- AWS.newEnv region AWS.Discover
+  runResourceT $ AWS.runAWST env $
     sequence [ ("vpc" .=)      <$> matchWithName infraEc2vpc
              , ("subnet" .=)   <$> match infraEc2subnet
              , ("sg" .=)       <$> match infraEc2sg
@@ -227,17 +220,11 @@ matchInfras infras@Infras{..} = object <$> do
              , ("elb" .=)      <$> match infraElb
              ]
   where
-    discover i t = toDiscovery <$> matchTags i t
+    match, matchWithName ::
+      (CanMatch a, Applicative f, MonadCatch f, MonadResource f)
+      => Attrs a -> AWST f (Attrs (Either DiscoveryError ResourceId))
+    match = traverse (`discover` [realmName])
+    matchWithName = Map.traverseWithKey (\k v -> discover v [realmName, ("Name", k)])
 
-    match ::
-      (CanMatch a, Applicative f, MonadCatch f, MonadError AWS.Error f, AWS.MonadAWS f)
-      => Attrs a -> f (Attrs (Either DiscoveryError ResourceId))
-    match = traverse (`discover` [("realm", infraRealmName)])
-
-    matchWithName ::
-      (CanMatch a, Applicative f, MonadCatch f, MonadError AWS.Error f, AWS.MonadAWS f)
-      => Attrs a -> f (Attrs (Either DiscoveryError ResourceId))
-    matchWithName = Map.traverseWithKey
-                   (\k v -> discover v [("realm", infraRealmName), ("Name", k)])
-
+    realmName = ("realm", infraRealmName)
     region = validateRegion infras
